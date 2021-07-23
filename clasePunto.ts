3009753080356f9b7a3ba6456669469991e7b1a6
Nicolilo11 <nicoramitos@gmail.com>
2021-07-23 02:55:51 +0200

    fix: git rebase

--- clasePunto.ts
+++ clasePunto.ts
@@ -9,46 +9,54 @@ export class Punto
         this.y = y;
     }
 
-    getX():number
+    public getX():number
     {
         return this.x
     }
 
-    getY():number
+    public getY():number
     {
         return this.y
     }
 
-    setX(x:number):void
+    public setX(x:number):void
     {
         this.x = x;
     }
 
-    setY(y:number):void
+    public setY(y:number):void
     {
         this.x = y;
     }
 
-    toString()
+    public toString()
     {
         let texto = this.x.toString()+ "," + this.y.toString();
         return  "●" + "\t" + '"' + "(" + texto + ")" + '"'; 
     }
 
-    distanciaAlOrigen():number
+    public distanciaAlOrigen():number
     {
         let distanciaAlOrigen:number = parseFloat(((Math.hypot(this.getX(),this.getY())).toFixed(3)));
         return distanciaAlOrigen
     }
 
+<<<<<<< HEAD
     calcularDistancia(otroPunto:Punto):number
+=======
+    public calcularDistancia(otroPunto:Punto):number
+>>>>>>> fa8c4ca (feature<clasePunto.ts><claseTriangulo.ts><main.ts>: Crea clase triangulo, Private a metodos, Implementa metodos en main)
     {
         let calcularDistancia:number = parseFloat((Math.hypot((this.getX() - otroPunto.getX(),this.getY() - 
                                                  otroPunto.getY())).toFixed(3)));       
         return calcularDistancia
     }
 
+<<<<<<< HEAD
     calcularCuadrante():number
+=======
+    public calcularCuadrante():number
+>>>>>>> fa8c4ca (feature<clasePunto.ts><claseTriangulo.ts><main.ts>: Crea clase triangulo, Private a metodos, Implementa metodos en main)
     {
         let cuadrante:number = 0;
 
@@ -71,7 +79,11 @@ export class Punto
     return cuadrante
     }
 
+<<<<<<< HEAD
     calcularMasCercano(puntos:Punto[]):Punto
+=======
+    public calcularMasCercano(puntos:Punto[]):Punto
+>>>>>>> fa8c4ca (feature<clasePunto.ts><claseTriangulo.ts><main.ts>: Crea clase triangulo, Private a metodos, Implementa metodos en main)
     {
         let puntoReferencia:number = 0;
         let distancia:number = this.calcularDistancia(puntos[0]);
--- main.ts
+++ main.ts
@@ -1,9 +1,18 @@
 import { Punto } from './clasePunto';
+import { Triangulo } from './claseTriangulo';
 
 let punto1:Punto = new Punto(10,5);
+<<<<<<< HEAD
 let punto2:Punto = new Punto(500,200);
 let punto3:Punto = new Punto(600,400);
 let punto4:Punto = new Punto(24,30);
+=======
+let punto2:Punto = new Punto(15,10);
+let punto3:Punto = new Punto(20,15);
+let punto4:Punto = new Punto(25,20);
+
+let triangulo1:Triangulo = new Triangulo(punto2, punto3,punto4);
+>>>>>>> fa8c4ca (feature<clasePunto.ts><claseTriangulo.ts><main.ts>: Crea clase triangulo, Private a metodos, Implementa metodos en main)
 
 console.log(punto1.getX());
 console.log(punto1.getY());
@@ -12,3 +21,9 @@ console.log(punto1.calcularDistancia(punto2));
 console.log(punto1.calcularCuadrante());
 console.log(punto1.calcularMasCercano([punto2, punto3, punto4]));
 
+<<<<<<< HEAD
+=======
+console.log(triangulo1.calcularLongitudLados());
+
+
+>>>>>>> fa8c4ca (feature<clasePunto.ts><claseTriangulo.ts><main.ts>: Crea clase triangulo, Private a metodos, Implementa metodos en main)
